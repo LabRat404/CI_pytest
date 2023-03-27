e14e711a3904423e5bf68118cf04ee78a7cf3fa8
LabRat404 <ggwp12336@gmail.com>
2023-03-28 00:28:18 +0800

    init

---
+++ myApp.py
@@ -0,0 +1,23 @@
+from flask import Flask
+
+def check_type(my_name, my_id):
+    # names should exist
+    if not my_name:
+        return False
+    return True
+
+# imagine these data is from db
+myName = "Yeung Tang"
+myId = "1155144676" 
+### end of data
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    if check_type(myName, myId):
+        return f"<p> Hello, my name is {myName}, my sid is {myId}</p>"
+    else:
+        return f"<p> Hello, my name is unknow, my sid is unknow</p>"
+if __name__ == "__main__":
+    app.run(debug=True)
