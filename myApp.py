4adb1b9ed993b958cb0986d48e8697a4c4091330
LabRat404 <ggwp12336@gmail.com>
2023-03-31 14:37:55 +0800

    Done finishing Lab7, finished all the checks

--- myApp.py
+++ myApp.py
@@ -14,6 +14,46 @@ def check_type(my_name = None, my_id = None):
                 return False
         return True
 
+def check_name(my_name = None):
+    # names should exist
+    if not my_name:
+        return False
+    else:
+        if not isinstance(my_name, str):
+            raise TypeError('_str must be a string')
+        if (my_name[-1] == ' '):
+            return False
+        if (not my_name.isprintable):
+            return False
+        return True
+
+def check_name_len(my_name = None):
+    # names should exist
+    if not my_name:
+        return False
+    else:
+        if not isinstance(my_name, str):
+            raise TypeError('_str must be a string')
+        if (len(my_name.encode('utf-8'))>20):
+            return False
+        return True
+
+def check_sid_len(my_id = None):
+    # names should exist
+    if not my_id:
+        return False
+    if my_id:
+        #check if string !10 chracter
+        if (len(str(my_id))!=10):
+            return False
+        #check if contain space 
+        if (not  str(my_id).isdigit()):
+            return False
+        if not (isinstance(my_id, int) or (isinstance(my_id, str) and my_id.isdigit())):
+        #if not (isinstance(my_id, int)):
+            return False
+        #check length of the id
+        return True
 # imagine these data is from db
 myName = "Yeung Tang"
 myId = "1155144676" 
@@ -28,4 +68,4 @@ def hello_world():
     else:
         return f"<p> Hello, my name is unknow, my sid is unknow</p>"
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
--- test/test_app.py
+++ test/test_app.py
@@ -1,5 +1,7 @@
 from myApp import check_type
-
+from myApp import check_name
+from myApp import check_name_len
+from myApp import check_sid_len
 def test_check_type():
     assert check_type() == False
     assert check_type('test') == True
@@ -23,4 +25,34 @@ def test_check_type():
 
     myId4 = "a_john"
     assert check_type(myname, myId4) == False
+
+
+def test_check_name():
+    assert check_name("abc") == True
+    assert check_name("ab ") == False
+    assert check_name("\xaa\xaa") == True
+    assert check_name("\xaa \xaa") == True
+    assert check_name("\xaa \xaa") == True
+    assert check_name("\xaa \xfa ") == False
+    assert check_name("中文_is_printable") == True
+
+def test_check_name_len():
+    assert check_name_len("abc") == True
+    assert check_name_len("abc abc") == True
+    assert check_name_len("\xaa\xaa") == True
+    assert check_name_len("\xaa \xaa") == True
+    assert check_name_len("abc abc abc abc abc a") == False
+    assert check_name_len("\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa") == True
+    assert check_name_len("\xaa\xaa\xaa\xaa\xaa \xaa\xaa\xaa\xaa\xaa") == False
+
+
+def test_check_sid_len():
+    assert check_sid_len("1155") == False
+    assert check_sid_len("1155111111") == True
+    assert check_sid_len("115511111 ") == False
+    assert check_sid_len(115511111) == False
+    assert check_sid_len(1155111111) == True
+    assert check_sid_len("1155 11111") == False
+
+
 
